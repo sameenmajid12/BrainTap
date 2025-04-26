c2559e411038d3e5ff2ee5ca96efa793b72d1dd6
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-04-26 02:39:01 -0400

    Added Navigation container and createNativeStackNavigator to navigate between screens

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import StartScreen from './screens/StartScreen';
-import GameScreen from './screens/GameScreen';
+import {useState} from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import StartScreen from "./screens/StartScreen";
+import GameScreen from "./screens/GameScreen";
 const Stack = createNativeStackNavigator();
-export default function App() {
+export default function App({navigation}) {
+  const [gameStatus, setGameStatus] = useState('initial');
+  const [level, setLevel] = useState(0);
+  
+  const RestartGame=()=>{
+
+  }
+  const SaveScore=()=>{
+
+  }
   return (
-   <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={StartScreen}/>
-      <Stack.Screen name="Game" component={GameScreen}/>
-    </Stack.Navigator>
-   </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false}}
+          name="home"
+          children={(props)=><StartScreen {...props} setGameStatus={setGameStatus}/>}
+        />
+        <Stack.Screen
+          options={{ headerShown: false}}
+          name="game"
+          children={(props)=><GameScreen {...props} gameStatus={gameStatus} setGameStatus={setGameStatus}/>}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
+}
