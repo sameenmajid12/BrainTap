a49301c701823443b79fcb57752fbfa0121f1f09
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-04-26 02:39:43 -0400

    Implemented StartButton functionality

--- components/Buttons.js
+++ components/Buttons.js
@@ -1,27 +1,32 @@
-import { Pressable, TouchableOpacity,StyleSheet } from "react-native";
-import colors from '../colors';
-const StartButton=()=>{
-  <Pressable style={({pressed})=>[
-    styles.startButton,
-    {
-      backgroundColor: pressed?colors.accent:colors.text
-    }
-  ]}>
-    <Text style={styles.startButtonText}>Start Game</Text>
-  </Pressable>
-}
-const RestartButton=()=>{
-
-}
-const SaveButton=()=>{
-
-}
+import { Pressable, TouchableOpacity, StyleSheet, Text } from "react-native";
+import colors from "../colors";
+const StartButton = ({ StartGame }) => {
+  return (
+    <Pressable
+      onPress={StartGame}
+      style={({ pressed }) => [
+        styles.startButton,
+        {
+          backgroundColor: !pressed ? colors.accent : colors.text,
+        },
+      ]}
+    >
+      <Text style={styles.startButtonText}>Start Game</Text>
+    </Pressable>
+  );
+};
+const RestartButton = () => {};
+const SaveButton = () => {};
 const styles = StyleSheet.create({
-  startButton:{
-
+  startButton: {
+    width: 150,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  startButtonText:{
-
-  }
-})
-export {StartButton, RestartButton, SaveButton};
+  startButtonText: {
+    fontWeight: 500,
+  },
+});
+export { StartButton, RestartButton, SaveButton };
