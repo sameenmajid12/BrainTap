f21257a3230e0ea7d4190730230036dae11d3511
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-04-27 19:37:19 -0400

    Improved flip animations and added a color change for wrong guesses

--- components/Cards.js
+++ components/Cards.js
@@ -8,24 +8,39 @@ import {
   Pressable,
 } from "react-native";
 import colors from "../colors";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 
 const getNumCards = (level) => {
-  if (level >= 0 && level < 9) return 9;
-  if (level >= 9 && level < 16) return 16;
-  if (level >= 16 && level < 25) return 25;
-  if (level >= 25 && level < 36) return 36;
+  if (level >= 0 && level <= 9) return 9;
+  if (level > 9 && level <= 16) return 16;
+  if (level > 16 && level <= 25) return 25;
+  if (level > 25 && level <= 36) return 36;
   return 49;
 };
 
-const Cards = ({ level, gameStatus }) => {
+const Cards = ({
+  level,
+  gameStatus,
+  setGuessesLeft,
+  setWrongGuess,
+  wrongGuess,
+  setCurrentNumber,
+  currentNumber
+}) => {
   const numCards = getNumCards(level);
   const [flippedCards, setFlippedCards] = useState([]);
   const [numberedCardIndices, setNumberedCardIndices] = useState([]);
-  const [currentNumber, setCurrentNumber] = useState(0);
-  const flipAnimations = useRef(
-    Array.from({ length: numCards }, () => new Animated.Value(180))
-  ).current;
+  const [animations, setAnimations] = useState({
+    flipAnimations: [],
+    cardColors: []
+  });
+
+  useEffect(() => {
+    setAnimations({
+      flipAnimations: Array.from({ length: numCards }, () => new Animated.Value(180)),
+      cardColors: Array.from({ length: numCards }, () => new Animated.Value(0))
+    });
+  }, [numCards]);
 
   useEffect(() => {
     if (gameStatus === "showcards") {
@@ -34,25 +49,26 @@ const Cards = ({ level, gameStatus }) => {
     } else if (gameStatus === "hidecards") {
       setFlippedCards([]);
       flipAllCards(false);
+      setCurrentNumber(1);
     }
-  }, [gameStatus]);
+  }, [gameStatus, numCards]);
 
   useEffect(() => {
     const arr = [];
-    while (arr.length < Math.min(level + 1, numCards)) {
+    while (arr.length < Math.min(level, numCards)) {
       const randomIndex = Math.floor(Math.random() * numCards);
       if (!arr.includes(randomIndex)) {
         arr.push(randomIndex);
       }
     }
     setNumberedCardIndices(arr);
-  }, [level]);
+  }, [level, numCards]);
 
   const flipAllCards = (show) => {
-    flipAnimations.forEach((anim, index) => {
+    animations.flipAnimations.forEach((anim, index) => {
       Animated.timing(anim, {
-        toValue: show ? 0: 180,
-        duration: 100+(index+1)*50,
+        toValue: show ? 0 : 180,
+        duration: 100 + (index + 1) * 50,
         useNativeDriver: true,
         easing: Easing.linear,
       }).start();
@@ -60,11 +76,34 @@ const Cards = ({ level, gameStatus }) => {
   };
 
   const flipCard = (index) => {
-    if(gameStatus!=='hidecards'){
+    if (gameStatus !== "hidecards") {
       return;
     }
     const isCurrentlyFlipped = flippedCards.includes(index);
-    Animated.timing(flipAnimations[index], {
+    if (
+      numberedCardIndices.indexOf(index) + 1 !== currentNumber &&
+      !isCurrentlyFlipped
+    ) {
+      setGuessesLeft((prev) => prev - 1);
+      setWrongGuess(true);
+      Animated.sequence([
+        Animated.timing(animations.cardColors[index], {
+          toValue: 1,
+          duration: 200,
+          useNativeDriver: true,
+        }),
+        Animated.timing(animations.cardColors[index], {
+          toValue: 0,
+          duration: 200,
+          useNativeDriver: true,
+        }),
+      ]).start();
+      return;
+    } else {
+      setCurrentNumber((prev) => prev + 1);
+      setGuessesLeft(3);
+    }
+    Animated.timing(animations.flipAnimations[index], {
       toValue: isCurrentlyFlipped ? 180 : 0,
       duration: 300,
       useNativeDriver: true,
@@ -88,48 +127,79 @@ const Cards = ({ level, gameStatus }) => {
   const cardDim = (availableWidth - (cardsPerRow - 1) * gapSize) / cardsPerRow;
 
   return (
-    <View style={styles.cardsContainer}>
-      {Array.from({ length: numCards }, (_, index) => {
-        const number = numberedCardIndices.includes(index)
-          ? numberedCardIndices.indexOf(index) + 1
-          : null;
+    <>
+      <View style={styles.cardsContainer}>
+        {Array.from({ length: numCards }, (_, index) => {
+          const number = numberedCardIndices.includes(index)
+            ? numberedCardIndices.indexOf(index) + 1
+            : null;
 
+          if (!animations.flipAnimations[index] || !animations.cardColors[index]) {
+            return null;
+          }
 
-        const frontAnimatedStyle = {
-          transform: [{ rotateY: flipAnimations[index].interpolate({
-            inputRange:[0, 180],
-            outputRange:['0deg', '180deg']
-          }) }],
-        };
+          const frontAnimatedStyle = {
+            transform: [
+              {
+                rotateY: animations.flipAnimations[index].interpolate({
+                  inputRange: [0, 180],
+                  outputRange: ["0deg", "180deg"],
+                }),
+              },
+            ],
+          };
 
-        const backAnimatedStyle = {
-          transform: [{ rotateY: flipAnimations[index].interpolate({
-            inputRange: [0, 180],
-            outputRange: ["180deg", "360deg"],
-          }) }],
-        };
+          const backAnimatedStyle = {
+            transform: [
+              {
+                rotateY: animations.flipAnimations[index].interpolate({
+                  inputRange: [0, 180],
+                  outputRange: ["180deg", "360deg"],
+                }),
+              },
+            ],
+          };
+          const backColor = {
+            backgroundColor: animations.cardColors[index].interpolate({
+              inputRange: [0, 1],
+              outputRange: ["white", "#FF0032"],
+            }),
+          };
+          return (
+            <Pressable onPress={() => flipCard(index)} key={index}>
+              <View
+                style={[
+                  styles.cardContainer,
+                  { width: cardDim, height: cardDim },
+                ]}
+              >
+                <Animated.View
+                  style={[styles.card, styles.cardFront, frontAnimatedStyle]}
+                >
+                  {number !== null && (
+                    <Text style={styles.cardText}>{number}</Text>
+                  )}
+                </Animated.View>
 
-        return (
-          <Pressable onPress={() => flipCard(index)} key={index}>
-            <View style={[styles.cardContainer, { width: cardDim, height: cardDim }]}>
-              <Animated.View style={[styles.card, styles.cardFront, frontAnimatedStyle]}>
-                {number !== null && (
-                  <Text style={styles.cardText}>{number}</Text>
-                )}
-              </Animated.View>
-
-              <Animated.View style={[styles.card, styles.cardBack, backAnimatedStyle]} />
-            </View>
-          </Pressable>
-        );
-      })}
-    </View>
+                <Animated.View
+                  style={[
+                    styles.card,
+                    styles.cardBack,
+                    backAnimatedStyle,
+                    { ...backColor },
+                  ]}
+                />
+              </View>
+            </Pressable>
+          );
+        })}
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   cardsContainer: {
-    flex: 0.6,
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "center",
@@ -137,7 +207,7 @@ const styles = StyleSheet.create({
     rowGap: 10,
   },
   cardContainer: {
-    perspective: 1000, 
+    perspective: 1000,
   },
   card: {
     position: "absolute",
